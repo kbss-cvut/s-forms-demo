fd141ccca805ee72c17a78b29ea94a2149db8fde
Max Chopart <max.chopart@gmail.com>
2022-10-24 19:26:30 +0200

    Changes to make app work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import SForms from "@kbss-cvut/s-forms";
+
+import form from "./form1.json"
+import {Constants} from "@kbss-cvut/s-forms";
+import "@kbss-cvut/s-forms/dist/s-forms.css";
+
+const modalProps = {
+  show: true,
+  title: "Title",
+};
+
+const options = {
+  i18n: {
+    "wizard.next": "Next",
+    "wizard.previous": "Previous",
+    "section.expand": "Expand",
+    "section.collapse": "Collapse",
+  },
+  intl: {
+    locale: "cs",
+  },
+  modalView: false,
+  modalProps,
+  horizontalWizardNav: true,
+  wizardStepButtons: true,
+  enableForwardSkip: true,
+  "startingQuestionId": "http://vfn.cz/ontologies/vita-study/form/vs--assesment-evaluation-q-NOTEXISTS",
+  startingStep: 1,
+  debugMode: true,
+  users: [
+    { id: "http://fel.cvut.cz/people/max-chopart", label: "Max Chopart" },
+    {
+      id: "http://fel.cvut.cz/people/miroslav-blasko",
+      label: "Miroslav Blasko",
+    },
+  ],
+  currentUser: "http://fel.cvut.cz/people/max-chopart",
+  icons: [
+    {
+      id: Constants.ICONS.QUESTION_HELP,
+      behavior: Constants.ICON_BEHAVIOR.ENABLE,
+    }
+  ],
+};
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <SForms form={form} options={options}/>
   );
 }
 
