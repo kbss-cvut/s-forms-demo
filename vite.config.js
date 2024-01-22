4662f1d033a5a1f0f9c49d897047180ca7862681
Max Chopart <max.chopart@gmail.com>
2024-01-22 10:49:37 +0100

    [Upd] Adding .env compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import envCompatible from "vite-plugin-env-compatible";
 
 export default defineConfig({
-  // depending on your application, base can also be "/"
-  base: '',
-  plugins: [react()],
-})
+  base: "",
+  plugins: [
+    react(),
+    envCompatible()
+  ],
+  define: {
+    "process.env": process.env
+  }
+});
